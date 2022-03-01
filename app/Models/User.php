2680deb968e94c4bef52c638a0984b05701b5275
Andrej Vasarovskij <andrej.vasarovskij@gmail.com>
2022-03-01 18:41:01 +0200

    Admin panel installed

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -10,35 +9,25 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    const ADMIN_ROLE_FK = 1;
+    const ROLE_TITLE_MAP = [
+        1 => 'Админ',
+        2 => 'Пользователь'
+    ];
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $table = 'users';
+    protected $guarded = [];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    public function isAdmin():bool{
+        return intval($this->role_fk) === self::ADMIN_ROLE_FK;
+    }
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function getRoleTitle():string{
+        return (isset(self::ROLE_TITLE_MAP[$this->role_fk])) ? self::ROLE_TITLE_MAP[$this->role_fk] : 'Неизвестно';
+    }
+
+    public function getRoles():array{
+        return self::ROLE_TITLE_MAP;
+    }
 }
