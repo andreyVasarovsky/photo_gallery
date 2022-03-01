3fbda81058706622e9926d9c30e1cf9adc947211
Andrej Vasarovskij <andrej.vasarovskij@gmail.com>
2022-03-01 14:56:08 +0200

    Auth created

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/test', function () {
     return view('test');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
