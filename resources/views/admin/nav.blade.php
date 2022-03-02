024a9316628fd9c968d05ff6b6fc8e3019d988db
Andrej Vasarovskij <andrej.vasarovskij@gmail.com>
2022-03-02 21:37:07 +0200

    Admin nav update

--- resources/views/admin/nav.blade.php
+++ resources/views/admin/nav.blade.php
@@ -52,7 +52,7 @@
             <nav class="mt-2">
                 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                     <li class="nav-item">
-                        <a href="{{ route('admin.client.inсдыdex') }}" class="nav-link">
+                        <a href="{{ route('admin.client.index') }}" class="nav-link">
                             <i class="nav-icon fas fa-users"></i>
                             <p>Клиенты</p>
                         </a>
