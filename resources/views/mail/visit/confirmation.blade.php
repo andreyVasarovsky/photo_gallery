04a0a46b0c3b27ece117040ecbc5149a2c592308
Andrej Vasarovskij <andrej.vasarovskij@gmail.com>
2022-03-06 21:13:27 +0200

    Email blade updated

--- resources/views/mail/visit/confirmation.blade.php
+++ resources/views/mail/visit/confirmation.blade.php
@@ -1,4 +1,8 @@
 @component('mail::message')
-    Тест!
-    {{ $data['email'] }}
+    <div class="col-12">
+        Добрый день, <strong>{{ $data['full_name'] }}</strong>
+    </div>
+    <div class="col-12">
+        Ваш визит успешно зарегестрирован на {{ $data['date'] }}
+    </div>
 @endcomponent
